c67c3eb791622bf318edfdba7463297e207b694d
alwayshigh206 <105470372+alwayshigh206@users.noreply.github.com>
2023-05-06 00:30:36 +0300

    update dhcp

--- dhcp-setup.sh
+++ dhcp-setup.sh
@@ -2,28 +2,10 @@
 
 # Installing a DHCP server
 sudo apt-get update
-sudo apt-get install dhcpd
+sudo apt-get install -y dnsmasq
 
-# Configuring the DHCP server
-sudo cat << EOF > /etc/dhcpd.conf
-subnet 192.168.32.0 netmask 255.255.255.0 {
-  range 192.168.32.10 192.168.32.100;
-  option routers 192.168.32.1;
-  option domain-name-servers 8.8.8.8, 8.8.4.4;
-}
-EOF
+sudo cp dnsmasq.conf /etc/dnsmasq.conf
 
-# Configuring the eth1 interface
-sudo cat << EOF >> /etc/network/interfaces
-auto eth1
-iface eth1 inet static
-  address 192.168.32.1
-  netmask 255.255.255.0
-EOF
+sudo systemctl start dnsmasq
 
-# Restarting network settings
-sudo service networking restart
-
-# Starting a DHCP server
-sudo systemctl enable dhcpd
-sudo systemctl start dhcpd
+sudo systemctl enable dnsmasq

6b4cac99392c5547f190d2bf73a6c9fe030277ad
alwayshigh206 <105470372+alwayshigh206@users.noreply.github.com>
2023-05-05 15:32:25 +0300

    Create nat+routing.sh

---
+++ nat+routing.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Setting up IP addresses for eth0 and eth1
+sudo dhclient eth0
+sudo tee /etc/network/interfaces > /dev/null <<EOT
+auto lo
+iface lo inet loopback
+
+auto eth1
+iface eth1 inet static
+address 192.168.32.1
+netmask 255.255.255.0
+EOT
+
+# Enabling routing
+sudo sysctl -w net.ipv4.ip_forward=1
+
+# Configuring NAT
+sudo iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+sudo iptables -A FORWARD -i eth0 -o eth1 -m state --state RELATED,ESTABLISHED -j ACCEPT
+sudo iptables -A FORWARD -i eth1 -o eth0 -j ACCEPT
+
+# Saving iptables settings
+sudo sh -c "iptables-save > /etc/iptables.ipv4.nat"
+
+# Setting up autoloading of saved iptables settings
+sudo sed -i '/exit 0/d' /etc/rc.local
+sudo tee -a /etc/rc.local > /dev/null <<EOT
+iptables-restore < /etc/iptables.ipv4.nat
+exit 0
+EOT

53c84267d5dec54c6255b924cfb3bad680b11cc1
alwayshigh206 <105470372+alwayshigh206@users.noreply.github.com>
2023-05-06 00:25:52 +0300

    upd iptables

--- nat+routing.sh
+++ nat+routing.sh
@@ -21,6 +21,9 @@ sudo sysctl -w net.ipv4.ip_forward=1
 sudo iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
 sudo iptables -A FORWARD -i eth0 -o eth1 -m state --state RELATED,ESTABLISHED -j ACCEPT
 sudo iptables -A FORWARD -i eth1 -o eth0 -j ACCEPT
+sudo apt-get install -y iptables-persistent
+sudo systemctl enable netfilter-persistent
+sudo netfilter-persistent save
 
 # Saving iptables settings
 if [ -f /etc/iptables.ipv4.nat ]; then

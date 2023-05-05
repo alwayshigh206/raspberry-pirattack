e1a78bad8e5e6557f08a168c4c226e3753593ebf
alwayshigh206 <105470372+alwayshigh206@users.noreply.github.com>
2023-05-06 01:18:17 +0300

    upd

--- run.sh
+++ run.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-sudo chmod +x nat+routing.sh eth0-eth1-routing.sh dhcp-setup.sh
+sudo chmod +x nat+routing.sh eth0-eth1-routing.sh 
 sudo ./nat+routing.sh
 sudo ./eth0-eth1-routing.sh
-#sudo ./dhcp-setup.sh

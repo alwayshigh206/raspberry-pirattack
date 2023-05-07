516c3e6510fcbeaeabb2263ef219c7d87d09a3dc
alwayshigh206 <105470372+alwayshigh206@users.noreply.github.com>
2023-05-08 02:24:19 +0300

    refacor code

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-
-sudo chmod +x ./source/nat+routing.sh ./source/eth0-eth1-routing.sh 
+echo "Script needs sudo permissions!"
+sudo chmod +x ./source/nat+routing.sh
 sudo chmod +x ./source/dhcp-setup.sh
-sudo ./source/nat+routing.sh && sudo ./source/eth0-eth1-routing.sh
+sudo ./source/nat+routing.sh
 sudo ./source/dhcp-setup.sh
--- source/nat+routing.sh
+++ source/nat+routing.sh
@@ -39,3 +39,32 @@ iptables-restore < /etc/iptables.ipv4.nat
 exit 0
 EOT
 sudo systemctl restart networking
+
+# Get current IP address and default gateway for eth0
+IPADDR_ETH0=$(ip -4 addr show eth0 | grep -oP '(?<=inet\s)\d+(\.\d+){3}')
+GW_ETH0=$(ip -4 route show default | awk '/dev eth0/ {print $3}')
+
+# eth0-eth1 routing script:
+# Get current IP address for eth1
+IPADDR_ETH1=$(ip -4 addr show eth1 | grep -oP '(?<=inet\s)\d+(\.\d+){3}')
+if [[ -n "$IPADDR_ETH1" ]]; then
+    sudo ip route add $IPADDR_ETH1/24 dev eth1
+else
+    echo "Could not find valid IP address for eth1"
+fi
+
+# Check if eth1 is up, bring it up if necessary
+ETH1_STATUS=$(cat /sys/class/net/eth1/operstate)
+if [[ "$ETH1_STATUS" == "down" ]]; then
+    sudo ip link set eth1 up
+fi
+
+# Enable IP forwarding
+sudo sysctl -w net.ipv4.ip_forward=1
+
+# Set default route through eth0
+sudo ip route replace default via $GW_ETH0 dev eth0
+
+# Set route for eth1 network
+sudo ip route add $IPADDR_ETH1/24 dev eth1
+#sudo systemctl restart networking

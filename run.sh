897ae35d5535802934f1d50287884ab49f71ec12
alwayshigh206 <105470372+alwayshigh206@users.noreply.github.com>
2023-05-07 19:02:38 +0300

    update readme, run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 sudo chmod +x nat+routing.sh eth0-eth1-routing.sh 
-sudo chmod +x dhcp-setup.sh
-sudo ./nat+routing.sh
-sudo ./eth0-eth1-routing.sh
+sudo chmod +x dhcp-setup.sh wap-config.sh
+sudo ./nat+routing.sh && sudo ./eth0-eth1-routing.sh
 sudo ./dhcp-setup.sh
